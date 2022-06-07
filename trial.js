d648b6159ca43178beec37057ead68c7125621ed
sibusisiwe0405 <ndlovuseluleko0@gmail.com>
2022-06-07 21:13:09 +0200

    Initial

---
+++ trial.js
@@ -0,0 +1,70 @@
+
+// All items we'd like to add
+
+
+const navItems = [
+    { href: 'trial.html', text: 'Home'},
+    { href: 'about.html', text: 'About'  },
+    { href: 'styleGuide.html', text: 'Style Guide' },
+    { href: 'portfolio.html', text: 'Portfolo'},
+    { href: './BlogPosts/Blogs.html', text: 'Blogs',  },
+   
+
+];
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    rendeMenu();
+});
+
+// create elements
+const navElem = document.createElement("nav");
+navElem.className = "menu";
+const navList = document.createElement("ol");
+
+
+//run the Rendermenu function
+const rendeMenu = () => {
+    const pathPrefix=window.location.pathname==="/WSOA3028A_2006780/"?'.':'..'
+    const navBar = document.querySelector(".menu-item");
+
+    for (let i = 0; i < navItems.length; i++) {
+
+        let navItem = document.createElement("li");
+        navItem.className = "menu-item";
+        let navLink = document.createElement("a");
+        navLink.href = navItems[i].href;
+        navLink.innerHTML = navItems[i].text;
+        navItem.appendChild(navLink);
+
+
+        //if subitems exists , then render them.
+        let subItems = navItems[i].subItems;
+
+        if (!!subItems && !!subItems.length) { 
+            let subList = document.createElement("ol");
+            subItems.forEach(subItem => {
+                let subnavItem = document.createElement("li");
+                let subnavLink = document.createElement("a");
+                subnavItem.className = "menu-item";
+                subList.className = "sub-menu";
+               
+                subnavLink.href = subItem.href;
+                subnavLink.innerHTML = subItem.text;
+                subnavItem.appendChild(subnavLink);
+                subList.appendChild(subnavItem);
+            });
+
+            navItem.appendChild(subList);
+        }
+        // Add anchor to list item, and list item to list
+
+        navList.appendChild(navItem);
+        navElem.appendChild(navList);
+    }
+}
+
+// Add list to body (or anywhere else)
+window.onload = function () {
+    document.body.appendChild(navElem);
+}
+
