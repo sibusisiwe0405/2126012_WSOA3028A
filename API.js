80b2a7993c1c02e9581eb19246d1c64c67e9094d
sibusisiwe0405 <ndlovuseluleko0@gmail.com>
2022-07-04 01:52:37 +0200

    Final API
    I got the unsplash api to work

--- API.js
+++ API.js
@@ -1,44 +1,47 @@
-const input = document.getElementById('input');
-const grid = document.getElementsByClassName('grid')
-[0];
- 
-window.addEventListener('keydown', function(event){
-    if(event.key === "Enter")
-    loadImg();
-})
+document.querySelector("#input").addEventListener("keydown", function(event)  {
+    if (event.key === "Enter")
+      apiRequest();
+  });
+  
+  document.querySelector("#search").addEventListener("click", () => {
+      apiRequest();
+  });
+  
+ function apiRequest  ()  {
+  removeImages();
+    document.querySelector("#grid").textContent = "";
+    //API access 
+    const url = 'https://api.unsplash.com/search/photos?query='+input.value+'&per_page=10&client_id=0Z0fOr_p53gcCKEanQtOUq0jFCsKK49zCRtzJ4RIbU0';
 
-function loadImg(){
-    
-removeImages();
-const url = 'https://api.unsplash.com/search/photos/?query='+input.value+'&per_page=9&client_id=0Z0fOr_p53gcCKEanQtOUq0jFCsKK49zCRtzJ4RIbU0';
-
-fetch(url)
-
-.then(reponse =>{
-  if(response.Ok)
-  return response.json();
-  else
-  alert(response.status)
-})
-
-.then(data=>{
-  const imageNodes =[];
-  for(let i = 0; i < data.results.length;i++){
-imageNodes[i] = document.createElement('div');
-imageNodes[i].className = 'img'; 
-imageNodes[i].style.backgroundImage = 'url('+data.results[i].urls.raw+')';
-imageNodes[i].addEventListener('dbclick', function(){
-  window.open(data.results[i].links.download,'_blank');
-})
-grid.appendChild(imageNodes[i]);
+    fetch(url)
+  
+    .then(response => {
+      if (response.ok) return response.json();
+      else
+      alert(response.status)
+     })
+     .then(data => {
+        loadImages(data);
+     })
+  
+     .catch(error => console.log(error));   
   }
-})
-
-function removeImages(){
-window.onload = function (){
+  loadImages = (data) => {
+    for(let i = 0;i < data.results.length;i++){
+      let image = document.createElement("div");
+      image.className = "img";
+      image.style.backgroundImage = "url("+data.results[i].urls.raw + "" +")";
+      //save feature. remove if not working
+      image.addEventListener("dblclick", function(){
+        window.open(data.results[i].links.download, '_blank');
+      })
+      document.querySelector("#grid").appendChild(image);
+    }
+  }
+  function removeImages(){
+    window.onload = function (){
+        
+            grid.innerHTML = '';
+    }
     
-        grid.innerHTML = '';
-}
-
-}
-}
+    }
